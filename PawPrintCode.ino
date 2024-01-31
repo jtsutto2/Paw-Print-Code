2fb55063f7fe612b3942d30c7d29f562e419e097
jtsutto2 <158201315+jtsutto2@users.noreply.github.com>
2024-01-30 20:37:41 -0500

    Add files via upload

---
+++ PawPrintCode.ino
@@ -0,0 +1,117 @@
+#include "PawPrintHeader.h"
+
+void setup() { //runs once at beginning
+
+  int ScalesReady = FALSE;
+  int ButtonReady = FALSE;
+
+  pinMode(directionPin, OUTPUT);
+  pinMode(pwmPin, OUTPUT);
+  pinMode(brakePin, OUTPUT);
+
+  Serial.begin(9600); //baud rate 9600
+
+}
+
+void loop() { //runs constantly
+
+  if (ScalesReady == TRUE) { //poll device Scale
+    ScaleValue(); //run scale
+    WaterValue(); //run other scale
+  }
+
+  if (ButtonReady == TRUE) { //poll touch sensor
+    RunButton(); //dispense food
+  }
+
+  if (ScalesReady == FALSE) { //wait 2s then let system check scale
+    delay(2000); 
+    ScalesReady = TRUE;
+  }
+
+  if (ButtonReady == FALSE) {
+    //do something
+  }
+
+}
+
+void ScaleValue(void) {
+  int ScaleLast, ScaleCurrent, FoodEaten;
+  int FoodMax = 9999;
+  int FoodUpdate = FALSE;
+
+  ScaleLast = analogRead(FoodPin); //read scale
+  Serial.println("Food level is being read: ");
+  Serial.println(ScaleLast); //output value
+  delay(500); //wait 500ms
+  ScaleCurrent = analogRead(FoodPin); //read again
+  Serial.println("Food level is being read: ");
+  Serial.println(ScaleCurrent); //output value
+
+  if (FoodEaten >= FoodMax) {
+    FoodEaten = 0; //empty amount of food eaten (implies refill of container)
+  if (ScaleLast != ScaleCurrent) {
+    FoodEaten += ScaleCurrent - ScaleLast; //update the amount of Food Eaten
+    FoodUpdate = TRUE;
+    }
+  }
+
+  if (FoodUpdate == TRUE) {
+    Serial.println("Your pet has eaten ");
+    Serial.println(FoodEaten); //outputs value of Food Eaten
+    Serial.println("total grams of food.");
+    FoodUpdate = FALSE;
+  }
+
+}
+
+void WaterValue(void) {
+  int WaterLast, WaterCurrent, WaterDrank;
+  int WaterMax = 9999;
+  int WaterUpdate = FALSE;
+
+  WaterLast = analogRead(WaterPin); //read water level
+  Serial.println("Water level is being read");
+  delay(500); //wait 500ms
+  WaterCurrent = analogRead(WaterPin); //read again
+  Serial.println("Water level is being read");
+
+  if (WaterDrank >= WaterMax) {
+    WaterDrank = 0; //empty amount of water drank (implies refill of container)
+  if (WaterLast != WaterCurrent) {
+    WaterDrank += WaterCurrent - WaterLast; //update the amount of water drank
+    WaterUpdate = TRUE;
+    }
+  }
+
+  if (WaterUpdate == TRUE) {
+    Serial.println("Your pet has eaten ");
+    Serial.println(WaterDrank); //outputs value of water drank
+    Serial.println("total grams of food.");
+    WaterUpdate = FALSE;
+  }
+
+}
+
+void RunButton(void) {
+  //motor control
+  digitalWrite(directionPin, HIGH); //set direction to high
+  digitalWrite(brakePin, LOW); //release brake
+  analogWrite(pwmPin, 50); //set pwm to 50% duty cycle (medium speed)
+
+  delay(2000); //run for 2 seconds
+
+  digitalWrite(brakePin, HIGH); //activate brake
+  analogWrite(pwmPin, 0); //set pwm to 0% (off)
+  delay(500); //cooldowm 0.5 seconds
+
+  //take picture
+
+  PictureProcess(); //reconscruct and send picture
+}
+
+void PictureProcess(void) {
+  //code to reconstruct picture
+
+  //send serial out
+}
---
+++ PawPrintHeader.h
@@ -0,0 +1,18 @@
+//values
+#define TRUE 1
+#define FALSE 0
+
+//sensor (analog???) pins
+const int FoodPin 9; //analog GPIO pin 9
+const int WaterPin 10; //analog GPIO pin 10
+
+//motor pins
+const int directionPin = 12; //Digital GPIO D6
+const int pwmPin = 3; //AREF for PWM
+const int brakePin = 9; //Digital GPIO D9
+
+//function defines
+void ScaleValue(void);
+void RunButton(void);
+void WaterValue(void);
+void PictureProcess(void);

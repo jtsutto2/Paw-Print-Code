b3544fbad145ca3b830cc53889789b230b4a41ea
Fares Elmetwally <90434413+faresash@users.noreply.github.com>
2024-02-16 16:17:53 -0500

    Update scales.ino

--- scales.ino
+++ scales.ino
@@ -2,11 +2,15 @@
 #include "mbed.h"
 #include <mbed_mktime.h>
 
+/***************************/
 // Global Variables //
+/***************************/
 
 int FoodEaten = 0; // Sum of all weight unloaded (food consumed) from food scale
 
+/***************************/
 // Scale-Related Functions //
+/***************************/
 
 //This is the function called from main for all food-related operations
 void food(void) {
@@ -18,7 +22,7 @@ void food(void) {
   int secondReading = readScale();           // Read again for the second value
   printScale(secondReading);                 // Display reading on console
 
-  if (didPetEat(firstReading, secondReading)) foodUpdate(); // If there is a decrease in scale weight (food was eaten), make necessary updates
+  if (didPetEat(firstReading, secondReading)) foodUpdate(secondReading - firstReading); // If there is a decrease in scale weight (food was eaten), make necessary updates
 }
 
 // Reads and returns value from scale
@@ -30,27 +34,28 @@ int readScale(void) {
 void printScale (int value) {
   Serial.println("Food level: ");
   Serial.println(value);                     // Output value
+  
   Serial.println("getLocalTime()");          // Print current timestamp for troubleshooting purposes
 }
 
 // Did the pet eat?
 bool didPetEat(int first, int second) {
-  if (second > first) {
-      FoodEaten += second - first;    //update the amount of Food Eaten
-      return TRUE;
-  }
+  if (second > first) return TRUE;
   else return FALSE;
 }
 
-void foodUpdate (void) {
-  Serial.println("Your pet has eaten ");
-  Serial.println(FoodEaten); //outputs value of Food Eaten
-  Serial.println("total grams of food.");
-  FoodUpdate = FALSE;
-}
+// Add to the total amount eaten
+void foodUpdate(int amountEaten) {
+  FoodEaten += amountEaten;                  // Update the amount of Food Eaten
   
+  Serial.println("Your pet has eaten ");
+  Serial.println(FoodEaten);                 // Outputs value of Food Eaten
+  Serial.println("total grams of food.");
 
-  if (FoodUpdate == TRUE) {
-    
-  }
+  Serial.println("getLocalTime()");          // Print current timestamp for troubleshooting purposes
+}
+
+// Reset FoodEaten value (probably after the day is over or something)
+void resetFoodEaten(void) {
+  FoodEaten = 0;
 }

b0d4ca138beda10b1dcfc108982d43874a05f777
Fares Elmetwally <90434413+faresash@users.noreply.github.com>
2024-02-16 12:17:31 -0500

    Update PawPrintCode.ino

--- PawPrintCode.ino
+++ PawPrintCode.ino
@@ -43,38 +43,6 @@ void loop() { //runs constantly
 
 }
 
-void WaterValue(void) {
-  int WaterLast, WaterCurrent, WaterDrank, WaterReset;
-  int WaterMax = 9999;
-  int WaterUpdate = FALSE;
-
-  WaterLast = analogRead(WaterPin); //read water level
-  Serial.println("Water level is being read");
-  Serial.println("getLocalTime()"); //print current time
-  delay(500); //wait 500ms
-  WaterCurrent = analogRead(WaterPin); //read again
-  Serial.println("Water level is being read");
-
-  if (WaterReset == TRUE) { //if reset command is given
-    WaterDrank = 0; //reset water
-  }
-  else if (WaterDrank >= WaterMax) {
-    Serial.println("ERROR: WaterDrank > WaterMax");
-  if (WaterLast != WaterCurrent) {
-    WaterDrank += WaterCurrent - WaterLast; //update the amount of water drank
-    WaterUpdate = TRUE;
-    }
-  }
-
-  if (WaterUpdate == TRUE) {
-    Serial.println("Your pet has eaten ");
-    Serial.println(WaterDrank); //outputs value of water drank
-    Serial.println("total grams of food.");
-    WaterUpdate = FALSE;
-  }
-
-}
-
 void RunButton(void) {
   int ButtonPressed;
 

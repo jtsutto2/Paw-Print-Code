442c9c02ba300529185077d30209fbf849ab86b0
jtsutto2 <158201315+jtsutto2@users.noreply.github.com>
2024-02-17 12:04:40 -0500

    Update PawPrintCodev2.ino

--- PawPrintCodev2.ino
+++ PawPrintCodev2.ino
@@ -1,10 +1,15 @@
 #include "PawPrintHeader.h"
+#include <Servo.h>
 
 //GLOBAL VARIABLES
 int FoodEaten, FoodReset;
 int WaterDrank, WaterReset;
 int ButtonState;
 int cooldownTime;
+
+//Servo Objects and fields
+Servo myservo;
+int speed;
 
 void setup() {
   
@@ -13,9 +18,11 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB
   }
 
-  pinMode(directionPin, OUTPUT);
-  pinMode(pwmPin, OUTPUT);
-  pinMode(brakePin, OUTPUT);
+  myservo.attach(9); //servo connected to pin 9
+  myservo.write(90); //90 is stopped
+  //0 is full speed clockwise
+  //90 is stopped
+  //180 is full speed counterclockwise
   
 }
 
@@ -41,45 +48,12 @@ void loop() {
 }
 
 void DispenseFood() {
+  
   //motor control
-  digitalWrite(directionPin, HIGH); //set direction to high
-  digitalWrite(brakePin, LOW); //release brake
-  analogWrite(pwmPin, 50); //set pwm to 50% duty cycle (medium speed)
-
+  myservo.write(135); //half speed counterclockwise
   delay(2000); //run for 2 seconds
+  myservo.write(90); //stop servo
 
-  digitalWrite(brakePin, HIGH); //activate brake
-  analogWrite(pwmPin, 0); //set pwm to 0% (off)
-  delay(500); //cooldowm 0.5 seconds
-}
-
-void WaterValue(void) {
-  int WaterLast, WaterCurrent,
-  int WaterMax = 9999;
-  int WaterUpdate = FALSE;
-
-  WaterLast = analogRead(WaterPin); //read water level
-  Serial.println("Water level is being read");
-  //Serial.println("getLocalTime()"); //print current time
-  delay(500); //wait 500ms
-  WaterCurrent = analogRead(WaterPin); //read again
-  Serial.println("Water level is being read");
-
-  if (WaterReset == TRUE) { //if reset command is given
-    WaterDrank = 0; //reset water
-  }
-  else if (WaterDrank >= WaterMax) {
-    Serial.println("ERROR: WaterDrank > WaterMax");
-  if (WaterLast != WaterCurrent) {
-    WaterDrank += WaterCurrent - WaterLast; //update the amount of water drank
-    WaterUpdate = TRUE;
-    }
-  }
-
-  if (WaterUpdate == TRUE) {
-    Serial.println("Your pet has eaten ");
-    Serial.println(WaterDrank); //outputs value of water drank
-    Serial.println("total grams of food.");
-    WaterUpdate = FALSE;
-  }
+  cooldownTime = millis() + 60000; //set 60 second cooldown
+    
 }

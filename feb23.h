55b4c90c5286dc93d169713c54d595a46fbc9f32
Fares Elmetwally <90434413+faresash@users.noreply.github.com>
2024-02-25 23:39:23 -0500

    Update feb23.h

--- feb23.h
+++ feb23.h
@@ -26,9 +26,11 @@
         void setupLoadCells(void);                          // This function must be called in the setup phase of main file
 
     // Scale-related functions
-        void readScales(void);                              // Reads the scales and populates Load Cell A and B
+        void readFoodScale(float &reading);                 // Reads the food scale
+        void readWaterScale(float &reading);                // Reads the water scale
         void tareOperations (void);                         // Used to zero or null out the scale reading if nothing is on it
         void printScale (float value);                      // Prints the value that was just weighed (value)
+        void resetReadings (void);                          // Used to reset ReadingA and ReadingB
 
     // Food-related functions
         void dispenseFood(void);                            // Runs the motors to dispense food
@@ -37,6 +39,12 @@
         void foodUpdate(float amountEaten);                 // Add to the total amount eaten
         void resetFoodEaten(void);                          // Reset FoodEaten value (probably after the day is over or something)
 
+    // Water-related functions
+        void waterOperations(void);                          // This is the function called from main for all food-related operations
+        bool didPetDrink(float first, float second);         // Did the pet eat?
+        void waterUpdate(float amountEaten);                 // Add to the total amount eaten
+        void resetWaterDrank(void);                          // Reset FoodEaten value (probably after the day is over or something)
+
     // Button-related functions
         bool checkButton (void);                            // Check if the pushbutton is pressed
 
@@ -44,22 +52,38 @@
         unsigned long currentTime(void);                    // Returns current timestamp in seconds
         unsigned long cooldown (unsigned long delay);       // Returns current time + delay for use as a cooldown
 
+    // Pet-related functions
+        float howMuchToEat(float weight);                   // Used to calculate how much food in grams a pet should eat daily based on bodyweight in pounds
+        float howMuchToDrink(float weight);                 // Used to calculate how much water in milliliters a pet should drink daily based on bodyweight in pounds
+        float consumptionProgress(void);                    // Used to calculate what percentage of food and water goals were met
+
 
 /***************************/
 // Global Variables //
 //*************************//
-
-bool flag1 = false; //temp
+    // Pet-related variables
+        float petWeight;
+        float dailyFoodAmount, dailyWaterAmount;
+        float foodProgress, waterProgress;
 
     // Food-related variables
         int WaterDrank, WaterReset;
         float foodEaten = 0;                                // Sum of all weight unloaded (food consumed) from food scale
+        float waterDrank = 0;
+        bool petAte = false;
+        bool petDrank = false;
 
     // Button-related variables
         int ButtonState;                                    // For reading and storing the pushbutton status
 
     // Cooldown-related variables
-        unsigned long foodCooldown, readingCooldown, foodEatenCooldown, myCooldown;
+        unsigned long foodCooldown, foodEatenCooldown;
+        unsigned long myCooldown;
+        unsigned long consumptionCooldown;
+        unsigned long readingCooldown;
+        int cycleCount;
+        bool isFirstReading = true;
+
 
     // Servo Objects and fields
         Servo myservo;
@@ -69,8 +93,8 @@ bool flag1 = false; //temp
         //Load Cells Pins
             const int HX711_dout_1 = 4;     //mcu > HX711 no 1 dout pin
             const int HX711_sck_1 = 5;      //mcu > HX711 no 1 sck pin
-            const int HX711_dout_2 = 6;     //mcu > HX711 no 2 dout pin
-            const int HX711_sck_2 = 7;      //mcu > HX711 no 2 sck pin
+            const int HX711_dout_2 = 8;     //mcu > HX711 no 2 dout pin
+            const int HX711_sck_2 = 9;      //mcu > HX711 no 2 sck pin
     
         // Josh's Load Cell Pins
             const int FoodPin = 9;          //analog GPIO pin 9

3e0e99fb0530b156750d0d21bb97ab5620049dbd
jtsutto2 <158201315+jtsutto2@users.noreply.github.com>
2024-04-19 12:46:27 -0400

    Update calculation_module.py

--- Python/calculation_module.py
+++ Python/calculation_module.py
@@ -1,35 +1,18 @@
 from datetime import datetime
 import time
 
-def calculation():
-  petName = 'Kirby'
-  targetWeight = 80
-  petWeight = 70;
+def calculation(petName, petWeight, targetWeight. foodEaten, waterEaten):
   dietFactor = targetWeight/petWeight #returns correction factor in diet relative to target weight
   #if targetWeight < petWeight then dietFactor < 1 to indicate less food
 
   foodGoal = petWeight * dietFactor * 27  # daily food goal adjusted for dietary needs
-  print(petName + " needs: " + str(foodGoal) + "g of food")
   waterGoal = petWeight * 30  # water should be based on current weight not target
-  print(petName + " needs: " + str(waterGoal) + "mL of water")
   
-  foodEaten = 0
-  waterDrank = 0
-
-  while True:
-      consumptionProgress(foodEaten, waterDrank, foodGoal, waterGoal, petName)
-      foodEaten += 10
-      waterDrank += 10
-      time.sleep(5)
-
+  consumptionProgress(foodEaten, waterDrank, foodGoal, waterGoal, petName)
 
 def consumptionProgress(foodEaten, waterDrank, foodGoal, waterGoal, petName):
   foodProgress = (foodEaten / foodGoal) * 100; #create percent of goal
   waterProgress = (waterDrank / waterGoal) * 100;
-  print(petName + " has eaten {:.2f}% of their daily food goal as of".format(foodProgress))
-  print(current_time())
-  print(petName + " has drank {:.2f}% of their daily water goal as of".format(waterProgress))
-  print(current_time())
 
   goalDict1 = { #creates a dictionary with the date: true/false format
     'Date MM/DD/YYYY': 'Goal met? (true/false)'
@@ -62,4 +45,3 @@ def current_time():
 def current_date():
   return datetime.now().strftime('%m-%d-%Y') #creates string with MM/DD/YYYY
   
-calculation()
